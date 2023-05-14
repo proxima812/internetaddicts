2ddf8df27ad43ba8a5233e66f3f61df36a042e38
Камиль Мирихан <samgold@MacBook-Air-Kamil.local>
2023-05-14 20:59:28 +0600

    nav link

--- src/consts.ts
+++ src/consts.ts
@@ -1,13 +1,13 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
-export const SITE_URL = 'https://internetaddicts.ru' // без /
-export const BANNER = '/image/banner_image.png'
+export const SITE_URL = "https://internetaddicts.ru"; // без /
+export const BANNER = "/image/banner_image.png";
 export const BASE_FILES = "https://t.me/all_12steps";
 
 export const TELEGRAM = "https://t.me/+2VhmiTsw1oYxYmIy";
-export const VK = 'https://vk.com/aiz_community';
-export const TIKTOK = '';
-export const YOUTUBE = 'https://www.youtube.com/channel/UC6X1MOn_So7iD2JEHkN-YHQ';
+export const VK = "https://vk.com/aiz_community";
+export const TIKTOK = "";
+export const YOUTUBE = "https://www.youtube.com/channel/UC6X1MOn_So7iD2JEHkN-YHQ";
 
-export const COPYRIGHT = 'Анонимные Интернет Зависимые';
-export const RKO = 'https://t.me/+WMn0QssSLHsyYzcy';
+export const COPYRIGHT = "Анонимные Интернет Зависимые";
+export const RKO = "https://t.me/+WMn0QssSLHsyYzcy";
