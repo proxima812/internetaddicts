891dfd16095c4143e420baafb9c3949f4fefd0de
Камиль Мирихан <samgold@MacBook-Air-Kamil.local>
2023-07-30 13:36:27 +0600

    123

--- src/consts.ts
+++ src/consts.ts
@@ -4,9 +4,9 @@ export const SITE_URL = "https://internetaddicts.ru"; // без /
 export const BANNER = "/image/banner_image.png";
 export const BASE_FILES = "https://t.me/all_12steps";
 
-export const TELEGRAM = "https://t.me/+2VhmiTsw1oYxYmIy";
-export const VK = "https://vk.com/aiz_community";
-export const TIKTOK = "";
+export const TELEGRAM = "https://t.me/aiz_itaa";
+export const VK = "https://vk.com/aiz_itta";
+export const TIKTOK = "https://t.me/aiz_itta";
 export const YOUTUBE = "https://www.youtube.com/channel/UC6X1MOn_So7iD2JEHkN-YHQ";
 
 export const COPYRIGHT = "Анонимные Интернет Зависимые";
