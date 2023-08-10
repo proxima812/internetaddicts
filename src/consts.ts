4f37919ff79bdf0681313936c584d90e7502800b
Камиль Мирихан <samgold@MacBook-Air-Kamil.local>
2023-08-10 11:47:54 +0600

    fix

--- src/consts.ts
+++ src/consts.ts
@@ -4,7 +4,7 @@ export const SITE_URL = "https://internetaddicts.ru"; // без /
 export const BANNER = "/image/banner_image.png";
 export const BASE_FILES = "https://t.me/all_12steps";
 
-export const TELEGRAM = "https://t.me/aiz_itaa";
+export const TELEGRAM = "https://t.me/aiz_itta";
 export const VK = "https://vk.com/aiz_itta";
 export const TIKTOK = "https://t.me/aiz_itta";
 export const YOUTUBE = "https://www.youtube.com/channel/UC6X1MOn_So7iD2JEHkN-YHQ";
