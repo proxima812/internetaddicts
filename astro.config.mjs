90b33ee88b1e0fff3f6c9ba22be583a4c9fdc518
Камиль Мирихан <samgold@MacBook-Air-Kamil.local>
2023-10-09 13:44:00 +0600

    delete img

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,39 +1,23 @@
-import image from "@astrojs/image";
-import mdx from "@astrojs/mdx";
-import partytown from "@astrojs/partytown";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
-import remarkToc from "remark-toc";
+import mdx from '@astrojs/mdx'
+import partytown from '@astrojs/partytown'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
- site: "https://internetaddicts.ru",
- compressHTML: true,
- integrations: [
-  mdx(),
-  partytown({
-   config: {
-    forward: ["dataLayer.push"],
-   },
-  }),
-  sitemap(),
-  tailwind({
-   applyBaseStyles: false,
-  }),
-  image({
-   serviceEntryPoint: "@astrojs/image/sharp",
-   logLevel: "debug",
-  }),
- ],
- markdown: {
-  remarkPlugins: [
-   [
-    remarkToc,
-    {
-     heading: "Содержание",
-    },
-   ],
+  site: 'https://internetaddicts.ru',
+  compressHTML: true,
+  integrations: [
+    mdx(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+    sitemap(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
   ],
- },
-});
+})
