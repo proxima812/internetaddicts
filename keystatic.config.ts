dda976d9cc30f63f6f92a250c8261deb4d50ebc1
proxima812 <mrzuc2015@yandex.ru>
2024-03-04 23:48:14 +0500

    1

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -8,7 +8,7 @@ export default config({
 		{
 			kind: "github",
 			repo: "itaaSite/internetaddicts",
-			// branchPrefix: "main/",
+			branchPrefix: "main/",
 		},
 	// : { kind: "local" },
 	collections: {
